e2d78f0b0b3d30f100c00335a2d7d9aa901c7bb5
sanderbk <sanderbaak@gmail.com>
2024-07-11 23:22:50 +0200

    push for hosting

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/BankAPI/"
+    baseURL: "https://backend-springboot-ykvy.onrender.com/BankAPI/"
 });
 export default instance;
