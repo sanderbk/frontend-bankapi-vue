04199e1fb7dd722d53a78556e4ffe537d3683b3e
sanderbk <sanderbaak@gmail.com>
2024-07-11 23:53:27 +0200

    lll

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+    transpileDependencies: ['vue'], // Adjust as needed based on your dependencies
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/frontend-bankapi-vue/'
+      ? '/rontend-bankapi-vue/'
       : '/'
-  }
+  };
